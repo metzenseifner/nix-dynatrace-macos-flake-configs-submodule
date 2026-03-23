e63cff5634cb0d927bb347d24674c87076c6771e
Jonathan L. Komar <Jonathan.Komar@dynatrace.com>
2026-03-23 06:55:19 +0100

    Update configs, reintroduce 03-config.zsh

--- nvim/snippets/http/gopass.lua
+++ nvim/snippets/http/gopass.lua
@@ -1,16 +1,21 @@
 return {
   s("gopass-minimal-example", fmt([[
+    ### run request with variable set via pre-request script
+
     # @lang=lua
     < {%
-      local secret = (function(h) local r=h:read("*a"); h:close(); return r end)(io.popen("gopass show -o path/to/secret"))()
+      local secret = (function(h) local r=h:read("*a"); h:close(); r = r:gsub("^%s+", ""):gsub("%s+$", ""); return r end)(io.popen("gopass show -o path/to/secret"))()
+      request.variables.set("secret", secret)
     %}
     ]], {}, { delimiters = "[]" })),
   s("gopass-full-example", fmt([[
+    ### run request with variable set via pre-request script
+
     # @lang=lua
     < {%
-      local secret = (function(h) local r=h:read("*a"); h:close(); return r end)(io.popen("gopass show -o path/to/secret"))()
+      local secret = (function(h) local r=h:read("*a"); h:close(); r = r:gsub("^%s+", ""):gsub("%s+$", ""); return r end)(io.popen("gopass show -o path/to/secret"))()
+      request.variables.set("secret", secret)
     %}
-    ###
 
     GET /resource
     Host: https://
